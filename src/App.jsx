e13bea70a67d7ce7e1fc08d7146bdf61bf2c34b1
ankitporiya <123635813+ankitporiya@users.noreply.github.com>
2024-08-04 15:01:15 +0530

    updated navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import NavBar from "../components/NavBar.jsx";
+// import Demo from "../components/Demo.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+    {/* <Demo/> */}
       <NavBar/>
     </>
   );
