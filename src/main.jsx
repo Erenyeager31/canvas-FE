aa032525dbb945f94cd2b95ecc8b61b0f4e8af31
ankitporiya <123635813+ankitporiya@users.noreply.github.com>
2024-08-04 13:54:12 +0530

    AP testing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import store from './app/store'
+// import store from './app/store'
 import { Provider } from 'react-redux'
-// import store from './redux/store'
+import store from './redux/store'
 import demo from '../components/demo'
 
 // As of React 18
