75e9eee2f5dba687ff8fc0f6797fd7dad975de9d
ankitporiya <123635813+ankitporiya@users.noreply.github.com>
2024-12-19 13:03:51 +0530

    navbar as per figma done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import NavBar from "../components/NavBar.jsx";
-// import Demo from "../components/Demo.jsx";
+import Demo from "../components/Demo.jsx";
 function App() {
   const [count, setCount] = useState(0);
 
